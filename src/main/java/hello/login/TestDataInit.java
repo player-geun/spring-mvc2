0184aef31d566ac5fbef9ce748d419167bc2f56b
player-geun <player_geun@naver.com>
2023-07-05 08:19:19 +0900

    feat : 로그인 기능 구현

--- src/main/java/hello/login/TestDataInit.java
+++ src/main/java/hello/login/TestDataInit.java
@@ -14,6 +14,7 @@ public class TestDataInit {
 
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
+
     /**
      * 테스트용 데이터 추가
      */
@@ -21,7 +22,7 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
-        
+
         Member member = new Member();
         member.setLoginId("test");
         member.setPassword("test!");
